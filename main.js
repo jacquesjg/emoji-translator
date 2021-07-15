2c628736c8a00bdc8b776af01adad79d2b5585d3
jacquesjg <jjeangi@gmail.com>
2021-07-15 20:01:38 +0200

    submission 2

--- main.js
+++ main.js
@@ -5,37 +5,47 @@ button.addEventListener('click', function () {
     // query input box, and save its value to variable
     // why doesn't query selector all work here
 
-    const textInput = document.querySelectorAll('input[type=text]');
+    const textInput = document.querySelector('input[type=text]');
     const textValue = textInput.value;
 
     // variable to store output
     const h1 = document.querySelector('#results')
 
-    // output function and save it to a variable
-    h1.innerText = encode(textValue);
-
-    // 
 
     // check radio buttons // why is the name of the value encode / whatever??
-    const checkedRadio = document.querySelectorAll('input[type=radio]:checked');
+    const checkedRadio = document.querySelector('input[type=radio]:checked');
+    console.log(checkedRadio)
+
     const radioValue = checkedRadio.value;
-
+    console.log(radioValue)
     if (radioValue === "encode") {
-        encode();
+        h1.innerText = encode(textValue);
 
-    } else if (radioValue === "translate") {
-        translate();
+    }
+
+    else if (radioValue === "translate") {
+        h1.innerText = translate(textValue);
     }
 
     else if (radioValue === "madlib") {
-        madlib();
+        h1.innerText = madlib(textValue);
     }
-
     else if (radioValue === "search") {
-        search();
+        h1.innerHTML = '';
+
+        //store the results of search function into an array
+        const postSearchEmojiArr = search(textValue)
+
+        // console.log(postSearchEmojiArr)
+
+        // loop through Array
+        for (const object of postSearchEmojiArr) {
+            // go in each object and grab the symbol that is found.
+        }
+
+
     }
 
-    console.log(search())
-
+    console.log(h1);
 });
 
